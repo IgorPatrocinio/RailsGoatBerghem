d45f855885e26ce7bac3784373869fb4644262a8
IgorPatrocinio <igornunesp@gmail.com>
2021-03-30 19:44:37 +0000

    Acess_admin

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -48,4 +48,8 @@ Railsgoat::Application.configure do
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
+  Rails.application.configure do
+  config.hosts.clear
+end
+
 end
